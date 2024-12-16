c7012d5fc02989ad75f44369b5c64236506c0a76
ACHii <>
2024-12-16 23:29:13 +0530

    user service

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"context"
-	"errors"
 
 	"github.com/achintha-dilshan/go-rest-api/internal/models"
 	"github.com/achintha-dilshan/go-rest-api/internal/repositories"
@@ -17,6 +16,7 @@ type UserService interface {
 	GetUserById(ctx context.Context, id int64) (*models.User, error)
 	UpdateUser(ctx context.Context, user *models.User) error
 	DeleteUser(ctx context.Context, id int64) error
+	ExistUserByEmail(ctx context.Context, email string) (bool, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
 }
 
@@ -26,71 +26,30 @@ func NewUserService(repository repositories.UserRepository) UserService {
 
 // create a new user
 func (s *userService) CreateUser(ctx context.Context, user *models.User) (int64, error) {
-	exists, err := s.repository.ExistUserByEmail(ctx, user.Email)
-
-	if err != nil {
-		return 0, err
-	}
-
-	if exists {
-		return 0, errors.New("email already exists")
-	}
-
 	return s.repository.CreateUser(ctx, user)
 }
 
 // get user by id
 func (s *userService) GetUserById(ctx context.Context, id int64) (*models.User, error) {
-	user, err := s.repository.GetUserById(ctx, id)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
+	return s.repository.GetUserById(ctx, id)
 }
 
 // update a user
 func (s *userService) UpdateUser(ctx context.Context, user *models.User) error {
-	existingUser, err := s.repository.GetUserById(ctx, user.Id)
-
-	if err != nil {
-		return errors.New("user not found")
-	}
-
-	if existingUser.Email != user.Email {
-		emailExists, err := s.repository.ExistUserByEmail(ctx, user.Email)
-
-		if err != nil {
-			return err
-		}
-
-		if emailExists {
-			return errors.New("email already exists")
-		}
-	}
-
 	return s.repository.UpdateUser(ctx, user)
 }
 
 // delete a user
 func (s *userService) DeleteUser(ctx context.Context, id int64) error {
-	_, err := s.repository.GetUserById(ctx, id)
-
-	if err != nil {
-		return errors.New("user not found")
-	}
-
 	return s.repository.DeleteUser(ctx, id)
+}
+
+// exist user by email
+func (s *userService) ExistUserByEmail(ctx context.Context, email string) (bool, error) {
+	return s.repository.ExistUserByEmail(ctx, email)
 }
 
 // get user by email
 func (s *userService) GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
-	user, err := s.repository.GetUserByEmail(ctx, email)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
+	return s.repository.GetUserByEmail(ctx, email)
 }
