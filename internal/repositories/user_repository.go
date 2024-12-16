c14cf98ef0319856feea0c48915028c5dc65a4e4
ACHii <>
2024-12-16 11:35:13 +0530

    refactor user repository

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -3,7 +3,7 @@ package repositories
 import (
 	"context"
 	"database/sql"
-	"fmt"
+	"errors"
 
 	"github.com/achintha-dilshan/go-rest-api/internal/models"
 )
@@ -31,12 +31,12 @@ func (r *userRepository) CreateUser(ctx context.Context, user *models.User) (int
 	result, err := r.db.ExecContext(ctx, query, user.Name, user.Email, user.Password)
 
 	if err != nil {
-		return 0, fmt.Errorf("error creating user: %w", err)
+		return 0, err
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		return 0, fmt.Errorf("error getting last insert ID: %w", err)
+		return 0, err
 	}
 
 	return id, nil
@@ -51,10 +51,11 @@ func (r *userRepository) GetUserById(ctx context.Context, id int64) (*models.Use
 	)
 
 	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil // No user found
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
 		}
-		return nil, fmt.Errorf("error getting user by ID: %w", err)
+
+		return nil, err
 	}
 
 	return &user, nil
@@ -66,7 +67,7 @@ func (r *userRepository) UpdateUser(ctx context.Context, user *models.User) erro
 	_, err := r.db.ExecContext(ctx, query, user.Name, user.Email, user.Id)
 
 	if err != nil {
-		return fmt.Errorf("error updating user: %w", err)
+		return err
 	}
 
 	return nil
@@ -78,7 +79,7 @@ func (r *userRepository) DeleteUser(ctx context.Context, id int64) error {
 	_, err := r.db.ExecContext(ctx, query, id)
 
 	if err != nil {
-		return fmt.Errorf("error deleting user: %w", err)
+		return err
 	}
 
 	return nil
@@ -91,7 +92,7 @@ func (r *userRepository) ExistUserByEmail(ctx context.Context, email string) (bo
 	err := r.db.QueryRowContext(ctx, query, email).Scan(&exists)
 
 	if err != nil {
-		return false, fmt.Errorf("error checking if user exists by email: %w", err)
+		return false, err
 	}
 
 	return exists == 1, nil
@@ -100,16 +101,17 @@ func (r *userRepository) ExistUserByEmail(ctx context.Context, email string) (bo
 // get user by email
 func (r *userRepository) GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
 	var user models.User
-	query := "SELECT id, name, email FROM users WHERE email = ?"
+	query := "SELECT id, name, email, password FROM users WHERE email = ?"
 	err := r.db.QueryRowContext(ctx, query, email).Scan(
-		&user.Id, &user.Name, &user.Email,
+		&user.Id, &user.Name, &user.Email, &user.Password,
 	)
 
 	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil // No user found
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
 		}
-		return nil, fmt.Errorf("error getting user by email: %w", err)
+
+		return nil, err
 	}
 
 	return &user, nil
