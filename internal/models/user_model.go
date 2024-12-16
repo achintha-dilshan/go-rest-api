1fca54943dac4badc924419ebb85876a20cd2df3
ACHii <>
2024-12-16 16:21:35 +0530

    remove validation login form user model

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -2,20 +2,13 @@ package models
 
 import (
 	"time"
-
-	"github.com/go-playground/validator/v10"
 )
 
 type User struct {
 	Id        int64     `json:"id"`
-	Name      string    `json:"name" validate:"required,min=3"`
-	Email     string    `json:"email" validate:"required,email"`
-	Password  string    `json:"-" validate:"required,min=3"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-}
-
-func (u *User) Validate() error {
-	validate := validator.New()
-	return validate.Struct(u)
 }
