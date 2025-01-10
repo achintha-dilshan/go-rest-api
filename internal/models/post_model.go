962403b29b47b5c2719b678f38afaae873527fe9
ACHii <>
2025-01-10 19:38:24 +0530

    post model

--- internal/models/post_model.go
+++ internal/models/post_model.go
@@ -4,6 +4,7 @@ import "time"
 
 type Post struct {
 	Id        int64     `json:"id"`
+	AuthorId  int64     `json:"author_id"`
 	Title     string    `json:"title"`
 	Body      string    `json:"body"`
 	CreatedAt time.Time `json:"createdAt"`
